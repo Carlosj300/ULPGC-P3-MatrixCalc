4d57cbb33458611fc318c1ea860d1533a4f3c97b
Alexander Álvarez Marques <31989083+alex222en@users.noreply.github.com>
2018-10-04 10:09:28 +0100

    Update matriz.adb

--- Proyecto/matriz.adb
+++ Proyecto/matriz.adb
@@ -1,5 +1,39 @@
 package body Matriz is
 
+   function sumar (m1 : Matriz; m2 : Matriz) return Matriz is
+     
+     -- Variables
+     result : Matriz (m1.f, m1.c);
    
+   begin
+      
+      -- Bucle para sumar posiciones
+      for y in m1.mat'Range(1) loop
+         for x in m1.mat'Range(2) loop
+            result.mat(y,x) := m1.mat(y,x) + m2.mat(y,x);
+         end loop;
+      end loop;
+   
+      -- Devolucion de datos
+      return result;
+   end sumar;
+   
+   function restar (m1 : Matriz; m2 : Matriz) return Matriz is
+     
+     -- Variables
+     result : Matriz (m1.f, m1.c);
+   
+   begin
+      
+      -- Bucle para sumar posiciones
+      for y in m1.mat'Range(1) loop
+         for x in m1.mat'Range(2) loop
+            result.mat(y,x) := m1.mat(y,x) - m2.mat(y,x);
+         end loop;
+      end loop;
+   
+      -- Devolucion de datos
+      return result;
+   end restar;
 
 end Matriz;
